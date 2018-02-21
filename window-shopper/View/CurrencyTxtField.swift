34ca8a31f2c7a8797cb98bc641fc8d43a86b0409
Victor Yamchinov <kodiers@gmail.com>
2018-02-21 03:00:42 +0300

    added button

--- window-shopper/View/CurrencyTxtField.swift
+++ window-shopper/View/CurrencyTxtField.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
 
     /*
@@ -18,8 +19,16 @@ class CurrencyTxtField: UITextField {
     }
     */
     
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
